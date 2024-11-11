3693601b698afdba5ff92ebfb042cb2071fdb2f2
XLordPlatypus <107401117+XLordPlatypus@users.noreply.github.com>
2024-11-11 12:16:07 +0100

    feat: Update home_setup.sh

--- structure/home_setup.sh
+++ structure/home_setup.sh
@@ -3,7 +3,7 @@
 cd /home/$LOGNAME/popos_setup/files
 cp .gitconfig /home/$LOGNAME/
 
-cd home/$LOGNAME
+cd /home/$LOGNAME
 sudo mkdir -p projects/{gamedev,webdev,other}
 sudo mkdir -p dev/{software,doc}
 
